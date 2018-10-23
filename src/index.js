b5319512525e076211d97e78b90ca1bc62c20095
Michael Thiessen <electromikenetic@gmail.com>
2018-10-23 15:33:09 -0400

    Exporting components properly

---
+++ src/index.js
@@ -0,0 +1,16 @@
+export { default as Renderless } from './components/Renderless.vue';
+export { default as Log } from './components/Log.vue';
+export { default as Interval } from './components/Interval.vue';
+export { default as Event } from './components/Event.vue';
+export { default as Combine } from './components/Combine.vue';
+
+// Cool pathname bro
+export { default as Component } from './components/component/Component.vue';
+export { default as Computed } from './components/component/Computed.vue';
+export { default as Lifecycle } from './components/component/Lifecycle.js';
+export { default as Watch } from './components/component/Watch.vue';
+
+export { default as List } from './components/data/List.vue';
+export { default as Set } from './components/data/Set.vue';
+export { default as State } from './components/data/State.vue';
+export { default as Toggle } from './components/data/Toggle.vue';
